a4942b6a5b114f74b7e581a47f9d277aa6797705
MAO Yuan (INTERN) <YMO2@dsone.3ds.com>
2017-08-29 11:36:05 +0200

    add delete

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,9 +1,29 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("hello from controller.js");
-    $http.get('/contactlist').then(function(response) {
-        console.log("i got the data i requested");
-        $scope.contactlist = response.data;
-    });
+    var refresh = function() {
+        $http.get('/contactlist').then(function(response) {
+            console.log("i got the data i requested");
+            $scope.contactlist = response.data;
+            $scope.contact = '';
+        });
+    };
+    refresh();
+    $scope.addContact = function() {
+        console.log($scope.contact);
+        $http.post('/contactlist', $scope.contact).then(function(response) {
+            console.log(response.data);
+            refresh();
+        });
+
+    };
+    $scope.remove = function(id) {
+        console.log(id);
+        $http.delete('/contactlist/' + id).then(function(response) {
+            console.log(response.data);
+            refresh();
+        });
+    }
+
 
 }]);
--- server.js
+++ server.js
@@ -1,25 +1,46 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
+var db = mongojs('contactlist', ['contactlist']);
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 app.get('/contactlist', function(req, res) {
     console.log("recu un requete GET");
-    person1 = {
-        name: 'yuan',
-        email: 'yuan355@gmail.com',
-        telephone: '11265446'
-    };
-    person2 = {
-        name: 'mao',
-        email: 'maon355@gmail.com',
-        telephone: '11288996'
-    };
-    person3 = {
-        name: 'vicky',
-        email: 'vicky355@gmail.com',
-        telephone: '18814556'
-    };
-    var contactlist = [person1, person2, person3];
-    res.json(contactlist);
-})
+    // person1 = {
+    //     name: 'yuan',
+    //     email: 'yuan355@gmail.com',
+    //     telephone: '11265446'
+    // };
+    // person2 = {
+    //     name: 'mao',
+    //     email: 'maon355@gmail.com',
+    //     telephone: '11288996'
+    // };
+    // person3 = {
+    //     name: 'vicky',
+    //     email: 'vicky355@gmail.com',
+    //     telephone: '18814556'
+    // };
+    // var contactlist = [person1, person2, person3];
+    db.contactlist.find(function(err, docs) {
+        console.log(docs);
+        res.json(docs);
+    });
+    //res.json(contactlist);
+});
+app.post('/contactlist', function(req, res) {
+    console.log(req.body);
+    db.contactlist.insert(req.body, function(err, docs) {
+        res.json(docs);
+    });
+});
+app.delete('/contactlist/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(id);
+    db.contactlist.remove({ _id: mongojs.ObjectId(id) }, function(err, docs) {
+        res.json(docs);
+    });
+});
 app.listen(3000);
 console.log("server running on port 3000");
