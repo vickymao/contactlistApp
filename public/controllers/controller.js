ccedfbd12e10b3159185054ee4f08a682d185cc7
MAO Yuan (INTERN) <YMO2@dsone.3ds.com>
2017-08-31 18:03:40 +0200

    todolist and contactlist show well,todolist can add and delete

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,11 +1,34 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+myApp.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            template: "<h1>Main</h1><p>Click on the links to change this content</p>"
+        }).when("/contactlistTool", {
+            templateUrl: "asset/contactlistTool.html"
+        }).when("/todoTool", {
+            templateUrl: "asset/todoTool.html"
+        });
+});
+myApp.controller('ToolsCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.contactlistTool = function(path) {
+        $location.path(path);
+    };
+    $scope.todoTool = function(path) {
+        $location.path(path);
+    };
+}]);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("hello from controller.js");
+    console.log("hello from contactcontroller");
     var refresh = function() {
         $http.get('/contactlist').then(function(response) {
             console.log("i got the data i requested");
             $scope.contactlist = response.data;
-            $scope.contact = '';
+            if ($scope.contact) {
+                $scope.contact.name = '';
+                $scope.contact.email = '';
+                $scope.contact.telephone = '';
+            }
+
         });
     };
     refresh();
@@ -23,7 +46,80 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
             console.log(response.data);
             refresh();
         });
-    }
+    };
+    $scope.edit = function(id) {
+        console.log(id);
+        $http.get('/contactlist/' + id).then(function(response) {
+            console.log(response.data);
+            $scope.contact = response.data;
 
+        });
+    };
 
+    $scope.update = function() {
+        console.log($scope.contact._id);
+        $http.put('/contactlist/' + $scope.contact._id, $scope.contact).then(function(response) {
+            console.log(response.data);
+            refresh();
+        });
+
+    };
+
+}]);
+myApp.controller('TodoCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("hello from todocontroller");
+    var refresh = function() {
+        $http.get('/todolist').then(function(response) {
+            console.log("i got the data i requested");
+            $scope.todolist = response.data;
+            if ($scope.todo) {
+                $scope.todo.title = '';
+                $scope.todo.beginning = '';
+                $scope.todo.deadline = '';
+                $scope.todo.description = '';
+
+            }
+
+        });
+    };
+    refresh();
+    $scope.addTodo = function() {
+        console.log($scope.todo);
+        $http.post('/todolist', $scope.todo).then(function(response) {
+            console.log(response.data);
+            refresh();
+        });
+
+    };
+    $scope.remove = function(id) {
+        console.log(id);
+        var txt;
+        var r = confirm("do you realy remove this todo?");
+        if (r == true) {
+            console.log('remove beginning');
+            $http.delete('/todolist/' + id).then(function(response) {
+                console.log(response.data);
+                console.log('remove done');
+                refresh();
+            });
+        }
+
+    };
+    // $scope.edit = function(id) {
+    //     console.log(id);
+    //     $http.get('/contactlist/' + id).then(function(response) {
+    //         console.log(response.data);
+    //         $scope.contact = response.data;
+
+    //     });
+    // };
+
+    $scope.update = function() {
+        // console.log($scope.contact._id);
+        // $http.put('/contactlist/' + $scope.contact._id, $scope.contact).then(function(response) {
+        //     console.log(response.data);
+        //     refresh();
+        // });
+
+    };
 }]);
--- server.js
+++ server.js
@@ -42,5 +42,45 @@ app.delete('/contactlist/:id', function(req, res) {
         res.json(docs);
     });
 });
+app.get('/contactlist/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(id);
+    db.contactlist.findOne({ _id: mongojs.ObjectId(id) }, function(err, docs) {
+        res.json(docs);
+    });
+});
+
+app.put('/contactlist/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(req.body.name);
+    db.contactlist.findAndModify({
+        query: { _id: mongojs.ObjectId(id) },
+        update: { $set: { name: req.body.name, email: req.body.email, telephone: req.body.telephone } },
+        new: true
+    }, function(err, docs) {
+        res.json(docs);
+    });
+});
+app.get('/todolist', function(req, res) {
+    console.log("recu un requete GET");
+
+    db.todolist.find(function(err, docs) {
+        console.log(docs);
+        res.json(docs);
+    });
+});
+app.post('/todolist', function(req, res) {
+    console.log(req.body);
+    db.todolist.insert(req.body, function(err, docs) {
+        res.json(docs);
+    });
+});
+app.delete('/todolist/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(id);
+    db.todolist.remove({ _id: mongojs.ObjectId(id) }, function(err, docs) {
+        res.json(docs);
+    });
+});
 app.listen(3000);
 console.log("server running on port 3000");
